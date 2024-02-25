c07612f201ba5f5cf31d2452cbc3c7662a70ec33
Akash Chauhan <akash.th4kur@gmail.com>
2024-02-26 01:38:26 +0530

    loading spinner fix

--- src/components/custom/Loader.tsx
+++ src/components/custom/Loader.tsx
@@ -1,7 +1,7 @@
 function Loading() {
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-[100]'>
-      <div className='animate-spin-slow ease-linear rounded-full border-dashed border-8 border-t-8 border-gray-200 h-6 w-6 sm:h-24 sm:w-24'></div>
+      <div className='animate-spin-slow ease-linear rounded-full border-dashed border-8 border-t-8 border-gray-200 h-6 w-6 sm:h-8 sm:w-8 md:h-16 md:w-16'></div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -68,6 +68,7 @@ export default {
         },
       },
       animation: {
+        "spin-slow": "spin 3s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
